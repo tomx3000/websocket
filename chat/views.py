4aaf88f164f9389d5fd2271d808438d44f701579
tomx3000 <tomx3000@gmail.com>
2020-02-28 21:57:00 +0300

    chat_modified

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
+# @login_required
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
+    return render(request, 'chat/chat.html', {
+        'room_name':  room_name,
+        'username': request.user.username,
     })
 
+# mark_safe(json.dumps(value))
