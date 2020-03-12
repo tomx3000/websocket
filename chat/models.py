8c2c2ad002cb059cf378e19ad9725cd5933ed2a0
tomx3000 <tomx3000@gmail.com>
2020-03-12 09:14:13 +0300

    update

--- chat/models.py
+++ chat/models.py
@@ -4,14 +4,30 @@ from django.db import models
 # Create your models here.
 User = get_user_model()
 
+class Contacts(models.Model):
+    user = models.ForeignKey(User, related_name="friends", on_delete=models.CASCADE)
+    friends =models.ManyToMany("self",blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 class Message(models.Model):
-    author = models.ForeignKey(User, related_name="auithor_message", on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contacts, related_name="messages", on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author.username
+        return self.contact.user.username
 
-    def last_20_messages():
-        return Message.objects.order_by("-created_at").all()[:20]
+class Chat(models.Model):
+    participants = models.ManyToMany(Contacts,related_name="chats")
+    messages = models.ManyToMany(Message,blank=True)
+
+
+    def last_20_messages(self):
+
+        return self.messages.objects.order_by("-created_at").all()[:20]
         
+    
+    def __str__(slef):
+        return str(self.id)
